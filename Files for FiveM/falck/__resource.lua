06e85576b555c2b7d9fe6f97335f741ebe926d3d
Hjenning Hansen <46818813+hjenningHansen@users.noreply.github.com>
2019-02-06 20:24:05 +0100

    Add files via upload

---
+++ Files for FiveM/falck/__resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    's_m_m_paramedic_01.ydd',
+    's_m_m_paramedic_01.yft',
+    's_m_m_paramedic_01.ymt',
+    's_m_m_paramedic_01.ytd',
+}
